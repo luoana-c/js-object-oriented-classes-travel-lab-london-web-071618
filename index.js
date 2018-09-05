445e994c8976a400de8cd617d22420053227575b
Luoana Chirita <luoana.livadariu@gmail.com>
2018-09-05 11:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name
+    this.startDate = new Date(date)
+  }
+  
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear() -1 
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  
+  blocksTravelled() {
+    return  this.endingLocation.vertical - this.beginningLocation.vertical
+  }
+}
+
+// let route = new Route({horizontal: 'Park', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
